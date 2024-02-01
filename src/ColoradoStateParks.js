707551410195d44b0e2c29842436da81a783bee4
MungaiS <thukusteven@gmail.com>
2024-02-01 16:50:04 +0300

    Completed assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,17 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import * as RMFunctions from "./parks/RockyMountain";
+import { trees, wildlife} from './parks/RockyMountain';
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
   return <h1>Colorado State Parks!</h1>;
 }
+RMFunctions.wildlife();
+// => "Elk, Bighorn Sheep, Moose"
+
+wildlife();
+// > "Elk, Bighorn Sheep, Moose"
+
+console.log(trees)
